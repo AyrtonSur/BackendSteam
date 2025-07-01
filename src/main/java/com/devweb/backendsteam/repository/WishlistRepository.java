12c4351411f0b5400e1fa94b9050ef010ab74869
AyrtonSur <ayrtonsurica@gmail.com>
2025-07-01 20:47:46 -0300

    feat: adiconando findByUserId no repository

--- src/main/java/com/devweb/backendsteam/repository/WishlistRepository.java
+++ src/main/java/com/devweb/backendsteam/repository/WishlistRepository.java
@@ -2,6 +2,9 @@ package com.devweb.backendsteam.repository;
 
 import com.devweb.backendsteam.model.EmbeddedIds.WishlistId;
 import com.devweb.backendsteam.model.Wishlist;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WishlistRepository extends JpaRepository<Wishlist, WishlistId> {}
+public interface WishlistRepository extends JpaRepository<Wishlist, WishlistId> {
+	List<Wishlist> findByUserId(Long userId);
+}
